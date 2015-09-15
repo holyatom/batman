d7086c8c035f0b9a8e147e363e0a9f04e8024474
Dmitriy Melnik <d.b.melnik@yandex.ru>
2015-09-15 22:59:16 +0600

    Add followers controller

---
+++ server/controllers/followers.js
@@ -0,0 +1,61 @@
+import ModelController from '../base/model_controller';
+import User from '../models/user';
+import Follower from '../models/follower';
+
+
+export default class FollowersController extends ModelController {
+  constructor() {
+    super();
+    this.logPrefix = 'follows-controller';
+    this.urlPrefix = '/users/:username/followers';
+    this.Model = User;
+    this.auth = true;
+    this.actions = ['list'];
+  }
+
+  list (req, res, next) {
+    var { username } = req.params;
+
+    if (username === 'profile') {
+      username = req.user.username;
+    }
+
+    User.findOne({ username }, (err, doc) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (!doc) {
+        return this.notFound(res);
+      }
+
+      var followeeId = doc._id;
+      var currentDate = new Date();
+
+      var followerFilters = {
+        followee_id: followeeId,
+        started: { $gt: currentDate },
+        ended: { $or: [
+          { $lt: currentDate },
+          { $eq: null }
+        ]}
+      };
+
+      Follower.find(followerFilters, 'follower_id', (err, docs) => {
+        if (err) {
+          return next(err);
+        }
+
+        req.followerIds = docs;
+
+        super.list(req, res, next);
+      });
+    });
+  }
+
+  getCustomListFilters (req) {
+    return {
+      _id: { $in: req.followerIds }
+    };
+  }
+}
---
+++ server/models/follower.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import v from 'libs/validators';
+
+
+var schema = new mongoose.Schema({
+  follower_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: v.required()
+  },
+  followee_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: v.required()
+  },
+  started: {
+    type: Date,
+    required: v.required()
+  },
+  ended: {
+    type: Date
+  }
+});
+
+export default mongoose.model('Follower', schema);
